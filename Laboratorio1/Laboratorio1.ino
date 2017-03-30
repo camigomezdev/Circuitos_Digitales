0ba8ee87e6ffe15f8939c8a617ccba36f50e7318
bonampa312 <bonampa312@gmail.com>
2017-03-30 10:26:36 -0500

    Etiquetas leds

--- Laboratorio1/Laboratorio1.ino
+++ Laboratorio1/Laboratorio1.ino
@@ -22,10 +22,11 @@ int pos=0;
 
 
 //LEDs a utilizar
-int led13 = 13;
-int lectura = 2;    
-int leer = 3;
-int led6 = 6;
+int led13 = 13; // led morse
+int led6 = 6; // Enable led
+// LDRs bonitos
+int lectura = 2; // Lectura de morse - resistencia
+int leer = 3; // Enable LDR
 
 //Estructura
 struct Caracter
